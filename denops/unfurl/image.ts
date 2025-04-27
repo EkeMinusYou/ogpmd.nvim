27023b46360aa1db2081bcbe663364197ef746ab
EkeMinusYou <euonymuslke@gmail.com>
2025-04-28 04:34:38 +0900

    feat(image): insert image as markdown link and fix insert mode

--- denops/unfurl/image.ts
+++ denops/unfurl/image.ts
@@ -3,9 +3,21 @@ import { Metadata } from "./metadata.ts";
 
 export const writeImage = async (denops: Denops, data: Metadata): Promise<void> => {
   if (data.type === "ogp" && data.imageUrl) {
-    await denops.cmd(`lua require("img-clip").paste_image({use_absolute_path = false, download_images = true}, "${data.imageUrl}")`);
+    console.log(data.imageUrl);
+    await denops.cmd(
+      `lua require("img-clip").paste_image({use_absolute_path = false, download_images = true, template = "![]($FILE_PATH)"}, "${data.imageUrl}")`,
+    );
+    // imgclipの後にinsertモードになってしまうので、stopinsertしておく
+    await denops.cmd("stopinsert");
+    await denops.cmd("normal 0");
   }
   if (data.type === "twitter" && data.tweetPhotoUrl) {
-    await denops.cmd(`lua require("img-clip").paste_image({use_absolute_path = false, download_images = true}, "${data.tweetPhotoUrl}")`);
+    console.log(data.tweetPhotoUrl);
+    await denops.cmd(
+      `lua require("img-clip").paste_image({use_absolute_path = false, download_images = true, template = "![]($FILE_PATH)"}, "${data.tweetPhotoUrl}")`,
+    );
+    // imgclipの後にinsertモードになってしまうので、stopinsertしておく
+    await denops.cmd("stopinsert");
+    await denops.cmd("normal 0");
   }
 };
