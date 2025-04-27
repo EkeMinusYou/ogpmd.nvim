85f0adefb20c1762019bde6d4878c0fa7e740907
EkeMinusYou <euonymuslke@gmail.com>
2025-04-28 04:18:17 +0900

    feat(image): add image pasting via img-clip

--- denops/unfurl/image.ts
+++ denops/unfurl/image.ts
@@ -3,9 +3,9 @@ import { Metadata } from "./metadata.ts";
 
 export const writeImage = async (denops: Denops, data: Metadata): Promise<void> => {
   if (data.type === "ogp" && data.imageUrl) {
-    await denops.call("append", ".", data.imageUrl);
+    await denops.cmd(`lua require("img-clip").paste_image({use_absolute_path = false, download_images = true}, "${data.imageUrl}")`);
   }
   if (data.type === "twitter" && data.tweetPhotoUrl) {
-    await denops.call("append", ".", data.tweetPhotoUrl);
+    await denops.cmd(`lua require("img-clip").paste_image({use_absolute_path = false, download_images = true}, "${data.tweetPhotoUrl}")`);
   }
 };
