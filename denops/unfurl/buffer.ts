640c9ec375feb2e0006a9ed69cb1c89179e6c29e
EkeMinusYou <euonymuslke@gmail.com>
2025-04-28 04:42:00 +0900

    feat(buffer): move cursor after writing to buffer

--- denops/unfurl/buffer.ts
+++ denops/unfurl/buffer.ts
@@ -4,5 +4,8 @@ export async function write(denops: Denops, data: string[]): Promise<void> {
   if (data.length === 0) {
     return;
   }
-  await denops.call("append", ".", data);
+  const currentLine = await denops.call("line", ".") as number;
+  await denops.call("append", currentLine, data);
+  const lastLine = currentLine + data.length;
+  await denops.call("cursor", lastLine, 1);
 }
